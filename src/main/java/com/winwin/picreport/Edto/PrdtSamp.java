657cedb674b4684efe275fc4398b7425ae99c4e0
hanpenghu <563909408@qq.com>
2017-12-13 09:42:51 +0800

    2017_12_13   weekday(3)   9:42:30

--- src/main/java/com/winwin/picreport/Edto/PrdtSamp.java
+++ src/main/java/com/winwin/picreport/Edto/PrdtSamp.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import com.winwin.picreport.Futils.ConstantInit;
 public class PrdtSamp {
 
     private String id="";
--- src/main/java/com/winwin/picreport/Futils/ConstantInit.java
+++ src/main/java/com/winwin/picreport/Futils/ConstantInit.java
@@ -1,7 +1,9 @@
 package com.winwin.picreport.Futils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
+@Order(1)
 @Component
 public class ConstantInit {
     @Value("${dirUrl}")
