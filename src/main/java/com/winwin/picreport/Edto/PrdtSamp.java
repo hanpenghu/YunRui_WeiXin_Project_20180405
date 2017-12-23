2a8c1e5e6e4385db302164e3528f0f00eafaf3b5
hanpenghu <563909408@qq.com>
2017-12-23 19:37:48 +0800

    2017_12_23   weekday(6)   19:37:42

--- src/main/java/com/winwin/picreport/Edto/PrdtSamp.java
+++ src/main/java/com/winwin/picreport/Edto/PrdtSamp.java
@@ -1,7 +1,5 @@
 package com.winwin.picreport.Edto;
-
 import com.winwin.picreport.AllConstant.Constant.ConstantInit;
-
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -9,38 +7,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-
 public class PrdtSamp {
-    private String id="";
+    private String id;//一条录入记录的唯一标识符号
 
-    private String prdCode="";//产品编码
+    private String prdCode;//产品编码
 
     private String idxName;//产品名称
 
     private String idxNo;//产品分类
 
-    private String markName;
+    private String markName;//品牌
 
-    private String markNo;
+    private String markNo;//品牌编号
 
-    private String colour;
+    private String colour;//颜色
 
-    private String size;
+    private String size;//尺寸
     //产品负责人
     private String salName;
     //产品负责人编码
     private String salNo;
 
-    private String cusNo;
+    private String cusNo;//客户编号
 
-    private String cusName;
+    private String cusName;//客户名称
 
-    private String isfenjie;
+    private String isfenjie;//是否分解//是否分解, y代表分解, n代表不分解
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    private Date sampMake;
+    private Date sampMake;//打样时间
 
-    private Date sampSend;
+    private Date sampSend;//----样品寄出时间
 
+
+
+    //打样时间戳  //前端传过来时间戳的时候变成Date赋给sampMake
     public Date getSampMake() {
         //        String res;
         //        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -70,6 +70,8 @@ public class PrdtSamp {
     }
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////使用转换字段转换过来的时间//////////////////////////////////////////////////////////////////
+
+    //样品寄出时间戳  //前端传过来时间戳的时候变成Date赋给sampSend
     public Date getSampSend() {
         if(this.sampSend!=null){//用于从数据库选出来的时候
             return this.sampSend;
@@ -97,21 +99,22 @@ public class PrdtSamp {
     /////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
-
-
-
-
-
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     private String sampMakeStamp="";//打样时间戳//传给我这个
     private String sampSendStamp="";//样品寄出时间戳//传给我这个
 
 
+
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //这个是转后的时间String格式,专门给徐勇用的在前端显示的
     private String sampMakeStr="";//给徐勇用
     private String sampSendStr="";//给徐勇用
 
@@ -155,15 +158,17 @@ public class PrdtSamp {
 
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //样品要求
     private String sampRequ="";
-
+    //样品描述
     private String sampDesc="";
-
+    //---缩略图名字包含的路径字符串,用;隔开,路径里面有!导致用户的缩略名字不能有!和;
     private String thum="";
 
+    //附件路径字符串,多个用;隔开,名字中的！做分隔符号
     private String attach="";
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+    //服务器的和图片所在的ip和域名,用于拼接附件和图片的地址
     private String dirUrl="http://10.0.3.101:8070/";
     //构造函数初始化网址
     public PrdtSamp() throws ParseException {
@@ -172,10 +177,12 @@ public class PrdtSamp {
 
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //该条记录插入时间,数据库自动会添加DEFAULT一个getdate()
     private Date insertdate;
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    //给徐勇显示在前端用的该条记录插入时间
     private String insertdateStr;
 
     public String getInsertdateStr() {
@@ -216,8 +223,9 @@ public class PrdtSamp {
     private BigDecimal littleorderprice;//财务小单费
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+    //该条记录的所有缩略图路径的集合,用上面的dirUrl来拼接
     private List<String> thumUrlList=new ArrayList<>();
+    //该条记录的所有附件路径的集合,用上面的dirUrl来拼接
     private List<String> attachUrlList=new ArrayList<>();
 
     public List<String> getThumUrlList() {
