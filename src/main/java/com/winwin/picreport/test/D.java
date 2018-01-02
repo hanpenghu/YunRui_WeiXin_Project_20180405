72d974049bba4264fb6946bb922d95f47b344343
hanpenghu <563909408@qq.com>
2018-01-02 15:42:01 +0800

    2018_1_2   weekday(2)   15:41:56

--- src/main/java/com/winwin/picreport/test/D.java
+++ src/main/java/com/winwin/picreport/test/D.java
@@ -1,8 +1,10 @@
 package com.winwin.picreport.test;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 public class D{
+    private BigDecimal bd;
     private Date date;
 
     public Date getDate() {
@@ -12,5 +14,23 @@ public class D{
     public D setDate(Date date) {
         this.date = date;
         return this;
+    }
+
+    public BigDecimal getBd() {
+        return bd;
+    }
+
+    public D setBd(BigDecimal bd) {
+        this.bd = bd;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("com.winwin.picreport.test.D{");
+        sb.append("bd=").append(bd);
+        sb.append(", date=").append(date);
+        sb.append('}');
+        return sb.toString();
     }
 }
