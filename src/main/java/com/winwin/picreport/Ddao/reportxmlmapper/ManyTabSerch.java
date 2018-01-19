eca0ad0ca5b2c6f9ea3e9f668907c743aae32d73
hanpenghu <563909408@qq.com>
2018-01-19 10:13:23 +0800

    2018_1_19   weekday(5)   10:13:12

--- src/main/java/com/winwin/picreport/Ddao/reportxmlmapper/ManyTabSerch.java
+++ src/main/java/com/winwin/picreport/Ddao/reportxmlmapper/ManyTabSerch.java
@@ -8,201 +8,221 @@ import java.util.*;
 public interface ManyTabSerch {
 
 
-        //得到数据库当前时间
-        @Select("SELECT GETDATE()")
-        Date getDate();
+    //得到数据库当前时间
+    @Select("SELECT GETDATE()")
+    Date getDate();
 
 
-        List<SalePrdDetailTab1> salePrdDetailTab1(@Param("chaXunTiaoJian") ChaXunTiaoJian chaXunTiaoJian,@Param("cus_no")String cus_no);//销货明细表
+    List<SalePrdDetailTab1> salePrdDetailTab1
+            (@Param("chaXunTiaoJian") ChaXunTiaoJian chaXunTiaoJian,
+             @Param("cus_no") String cus_no);//销货明细表
 
-        @Select("SELECT CUS_NO,NAME FROM CUST WHERE OBJ_ID=1")//1是客户//2是厂商
-        List<KeHu> getAllCustOf_obj_id_1_that_KeHuNotChangShang();
+    @Select("SELECT CUS_NO,NAME FROM CUST WHERE OBJ_ID=1")
+//1是客户//2是厂商
+    List<KeHu> getAllCustOf_obj_id_1_that_KeHuNotChangShang();
 
-        @Select("SELECT CUS_NO,NAME FROM CUST WHERE OBJ_ID=2")//1是客户//2是厂商
-        List<KeHu> getAll_obj_id_2_that_ChangShang();
+    @Select("SELECT CUS_NO,NAME FROM CUST WHERE OBJ_ID=2")
+//1是客户//2是厂商
+    List<KeHu> getAll_obj_id_2_that_ChangShang();
 
-        Integer updateMfPosNullToNothing001(@Param("mfPosWithBLOBs") MfPosWithBLOBs mfPosWithBLOBs);
-        Integer updateTfPosNullToNothing001(@Param("mfPosWithBLOBs") MfPosWithBLOBs mfPosWithBLOBs);
+    Integer updateMfPosNullToNothing001
+            (@Param("mfPosWithBLOBs") MfPosWithBLOBs mfPosWithBLOBs);
 
-
-        Integer update001OfOnlineBug();
-
-        Integer update002OfOnlineBug();
-
-        Integer update003OfOnlineBug();
-
-        Integer update004OfOnlineBug();
-
-        Integer update005OfOnlineBug();
-
-        Integer update006OfOnlineBug();
-        Integer update007_1_OfOnlineBug();
-        Integer update007_2_OfOnlineBug();
-        Integer update007_3_OfOnlineBug();
-        Integer update007_4_OfOnlineBug();
-
-//        @Update("update a set a.wh=b.chkw from tf_ck a,mf_ck_z b\n" +
-//                "where a.ck_no=b.ck_no and (b.CHKW is not NULL AND b.CHKW<>'')")
-//        Integer update008_1_OfOnlineBug();//这是原来的,后来变成下的了20171122变得
+    Integer updateTfPosNullToNothing001
+            (@Param("mfPosWithBLOBs") MfPosWithBLOBs mfPosWithBLOBs);
 
 
+    @Select("select osno,prdno,saphh,qty,sapph,caigouno,luohao,ganghao,realwidth,reallength,chengFenDaiMa " +
+            "from sapso where RTRIM(LTRIM(isnull(chengFenDaiMa,'')))=#{chengFenDaiMa} and RTRIM(LTRIM(isnull(osno,'')))=#{cusOsNo} and  RTRIM(LTRIM(isnull(prdno,'')))=#{prdNo}")
+    List<Sapso> select001(@Param("cusOsNo") String cusOsNo, @Param("prdNo") String prdNo,
+                          @Param("chengFenDaiMa") String chengFenDaiMa);
 
 
-        @Update({"update mf_pos set cus_os_no=os_no where os_id='SO' and (cus_os_no IS NULL or cus_os_no='') and os_dd>='2017-10-1'",
-                "update tf_pos set cus_os_no=os_no where os_id='SO' and (cus_os_no IS NULL or cus_os_no='') and os_dd>='2017-10-1'"})
-        Integer update008_1_OfOnlineBug();
-
-
-//        @Update("UPDATE A SET A.REM=B.SO_NO FROM MF_SQ A,TF_SQ B \n" +
-//                "WHERE A.CLS_ID='F' AND A.SQ_NO=B.SQ_NO AND B.ITM=1")
-//        Integer update009_1OfOnlineBug();
-//
-//        @Update("UPDATE B SET B.SO_NO=A.REM FROM MF_SQ A,TF_SQ B \n" +
-//                "WHERE A.CLS_ID='F' AND A.SQ_NO=B.SQ_NO AND (B.SO_NO is NULL or B.SO_NO='')")
-//        Integer update009_2OfOnlineBug();
-//
-//        @Update("UPDATE B SET B.est_dd=A.est_dd FROM MF_SQ A,TF_SQ B \n" +
-//                "WHERE A.SQ_NO=B.SQ_NO AND (B.EST_DD IS NULL)")
-//        Integer update009_3OfOnlineBug();
-
-
-
-
-        @Update("UPDATE  A SET A.REM=B.SO_NO FROM MF_SQ A,TF_SQ B \n" +
-                "WHERE A.CLS_ID='F' AND A.SQ_NO=B.SQ_NO AND B.ITM=1 AND (A.REM IS NULL or A.REM like '')")
-        Integer update009_1OfOnlineBug();
-
-
-        @Update("UPDATE  B SET B.SO_NO=A.REM FROM MF_SQ A,TF_SQ B \n" +
-                "WHERE A.CLS_ID='F' AND A.SQ_NO=B.SQ_NO AND (B.SO_NO is NULL or B.SO_NO='')")
-        Integer update009_2OfOnlineBug();
-
-        @Update("UPDATE  B SET B.est_dd=A.est_dd FROM MF_SQ A,TF_SQ B \n" +
-                "WHERE A.SQ_NO=B.SQ_NO AND (B.EST_DD IS NULL)")
-        Integer update009_3OfOnlineBug();
+    @Select("Select sal_no AS salNo,name AS name from salm")
+    List<YeWuYuan> getAllYeWuYuan();
 
 
 
 
 
+    ///////以下是onlinebug///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Delete("delete  from tf_pos_z where os_no not in (select os_no from mf_pos)")
+    Integer update010_1OfOnlineBug();
+
+    @Delete("delete  from mf_pos_z where os_no not in (select os_no from mf_pos)")
+    Integer update010_2OfOnlineBug();
+
+
+    Integer update001OfOnlineBug();
+
+    Integer update002OfOnlineBug();
+
+    Integer update003OfOnlineBug();
+
+    Integer update004OfOnlineBug();
+
+    Integer update005OfOnlineBug();
+
+    Integer update006OfOnlineBug();
+
+    Integer update007_1_OfOnlineBug();
+
+    Integer update007_2_OfOnlineBug();
+
+    Integer update007_3_OfOnlineBug();
+
+    Integer update007_4_OfOnlineBug();
+
+
+    @Update({"update mf_pos set cus_os_no=os_no where os_id='SO' and (cus_os_no IS NULL or cus_os_no='') and os_dd>='2017-10-1'",
+            "update tf_pos set cus_os_no=os_no where os_id='SO' and (cus_os_no IS NULL or cus_os_no='') and os_dd>='2017-10-1'"})
+    Integer update008_1_OfOnlineBug();
+
+
+    @Update("UPDATE  A SET A.REM=B.SO_NO FROM MF_SQ A,TF_SQ B \n" +
+            "WHERE A.CLS_ID='F' AND A.SQ_NO=B.SQ_NO AND B.ITM=1 AND (A.REM IS NULL or A.REM like '')")
+    Integer update009_1OfOnlineBug();
+
+
+    @Update("UPDATE  B SET B.SO_NO=A.REM FROM MF_SQ A,TF_SQ B \n" +
+            "WHERE A.CLS_ID='F' AND A.SQ_NO=B.SQ_NO AND (B.SO_NO is NULL or B.SO_NO='')")
+    Integer update009_2OfOnlineBug();
+
+    @Update("UPDATE  B SET B.est_dd=A.est_dd FROM MF_SQ A,TF_SQ B \n" +
+            "WHERE A.SQ_NO=B.SQ_NO AND (B.EST_DD IS NULL)")
+    Integer update009_3OfOnlineBug();
+
+
+    @Update("update  a set a.sup_prd_no=b.sup_prd_no from  tf_sq a,tf_pos b,mf_bom c,tf_bom d\n" +
+            " where \n" +
+            "a.so_no=b.os_no and b.prd_no=c.prd_no and c.bom_no=d.bom_no and a.prd_no=d.prd_no \n" +
+            " and (a.sup_prd_no IS NULL) and b.os_dd>='2017-10-1' and (b.sup_prd_no IS NOT NULL)")
+    Integer update009_4OfOnlineBug();
+
+
+    @Update("UPDATE A SET A.KHNBDH=C.KHNBDH FROM TF_POS_Z A,TF_POS B,TF_POS_Z C WHERE A.OS_ID='PO' AND A.OS_NO=B.OS_NO AND B.OTH_NO=C.OS_NO AND (A.KHNBDH IS NULL OR A.KHNBDH='') AND (C.KHNBDH IS NOT NULL OR C.KHNBDH != '')")
+    Integer gengXin_CaiGouDan_BiaoShen_KeHu_NeiBu_DanHao_OfOnlineBug();
 
 
 
-        @Select("select osno,prdno,saphh,qty,sapph,caigouno,luohao,ganghao,realwidth,reallength,chengFenDaiMa " +
-                "from sapso where RTRIM(LTRIM(isnull(chengFenDaiMa,'')))=#{chengFenDaiMa} and RTRIM(LTRIM(isnull(osno,'')))=#{cusOsNo} and  RTRIM(LTRIM(isnull(prdno,'')))=#{prdNo}")
-        List<Sapso> select001( @Param("cusOsNo") String cusOsNo, @Param("prdNo") String prdNo,@Param("chengFenDaiMa") String chengFenDaiMa);
+    @Update({"update prdt set formula='1;-1;-1;0;0;主单位数量/0.9144;副单位数量*0.9144' where ut='meters' and ut1='yards' and (formula IS NULL or formula='')",
+            "update prdt set formula='1;-1;-1;4;4;主单位数量*0.9144;副单位数量/0.9144' where ut='yards' and ut1='meters' and (formula IS NULL or formula='')",
+            "update prdt set formula='1;-1;-1;0;0;主单位数量/12;副单位数量*12' where ut='pcs' and ut1='Dozens' and (formula IS NULL or formula='')"})
+    Integer update011_1_OfOnlineBug();
 
-
-        @Delete("delete  from tf_pos_z where os_no not in (select os_no from mf_pos)")
-        Integer update010_1OfOnlineBug();
-
-        @Delete("delete  from mf_pos_z where os_no not in (select os_no from mf_pos)")
-        Integer update010_2OfOnlineBug();
-
-        @Select("Select sal_no AS salNo,name AS name from salm")
-        List<YeWuYuan> getAllYeWuYuan();
+    ////////////以上是onlinebug//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
-        @Update("update  a set a.sup_prd_no=b.sup_prd_no from  tf_sq a,tf_pos b,mf_bom c,tf_bom d\n" +
-                " where \n" +
-                "a.so_no=b.os_no and b.prd_no=c.prd_no and c.bom_no=d.bom_no and a.prd_no=d.prd_no \n" +
-                " and (a.sup_prd_no IS NULL) and b.os_dd>='2017-10-1' and (b.sup_prd_no IS NOT NULL)")
-        Integer update009_4OfOnlineBug();
-
-        @Select("SELECT ebNo FROM SAPSO WHERE RTRIM(LTRIM(ISNULL(osno,'')))+RTRIM(LTRIM(ISNULL(prdno,'')))+RTRIM(LTRIM(ISNULL(chengFenDaiMa,'')))=#{danHao_huoHao_chengFenDaiMa}")
-        List<String> selectEbNoFromSapso(@Param("danHao_huoHao_chengFenDaiMa") String danHao_huoHao_chengFenDaiMa);
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        @Select("select isnull(idx_no,'') as 'idxNo',name as 'idxName' from indx")
-        List<CategoryName> fenlei();
-
-
-        /**
-         *得到最高层级的分类,其实只有一个
-         * */
-        @Select("select isnull(idx_no,'') as 'idxNo',name as 'idxName' from indx where idx_up is null")
-        List<CategoryNameCode>getCommonder();
-
-        /**
-         *得到下一级的分类,有多个
-         * */
-        @Select("select isnull(idx_no,'') as 'idxNo',name as 'idxName' from indx where idx_up=#{indxNo}")
-        List<CategoryNameCode>getChildCategoryNameCode(@Param("indxNo") String indxNo);
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        @Select("select isnull(name,'') from prdt where idx1 in(select idx_no from indx where idx_no=#{idxNo})")
-        List<String> getCodeList(@Param("idxNo") String idxNo);
-
-        @Select("select isnull(sal_no,'') as 'salNo',isnull(name,'') as 'salName' from salm")
-         List<FuZeRen> fuZeRen();
-
-        @Select("select isnull(mark_no,'')as 'markNo',isnull(name,'')as 'name' from mark")
-        List<PinPai> pinPai();
-
-
-        List<String>selectDangQianYeSuoYouId
-                (@Param("dangQianYe") Integer dangQianYe
-                        ,@Param("meiYeXianShiShu")Integer meiYeXianShiShu);
-
-        /**
-         *得到某页分页id,打样未确认的
-         * */
-        List<String>selectDangQianYeSuoYouIdNotConfirm(@Param("dangQianYe") Integer dangQianYe,@Param("meiYeXianShiShu")Integer meiYeXianShiShu);
-
-        /**
-         *得到某页分页id,打样已经确认的
-         * */
-        List<String>selectDangQianYeSuoYouIdAlReadyConfirm(@Param("dangQianYe") Integer dangQianYe,@Param("meiYeXianShiShu")Integer meiYeXianShiShu);
-
-
-        @Select("select count(id) from PRDT_SAMP")
-        Integer dangYangZongJiLuShu();
 
 
 
-        @Update({"update prdt set formula='1;-1;-1;0;0;主单位数量/0.9144;副单位数量*0.9144' where ut='meters' and ut1='yards' and (formula IS NULL or formula='')",
-                "update prdt set formula='1;-1;-1;4;4;主单位数量*0.9144;副单位数量/0.9144' where ut='yards' and ut1='meters' and (formula IS NULL or formula='')",
-                "update prdt set formula='1;-1;-1;0;0;主单位数量/12;副单位数量*12' where ut='pcs' and ut1='Dozens' and (formula IS NULL or formula='')"})
-        Integer update011_1_OfOnlineBug();
 
-   @Select("Select count(id) from prdt_samp where isconfirm=1")
+    @Select("SELECT ebNo FROM SAPSO WHERE RTRIM(LTRIM(ISNULL(osno,'')))+" +
+            "RTRIM(LTRIM(ISNULL(prdno,'')))+" +
+            "RTRIM(LTRIM(ISNULL(chengFenDaiMa,'')))=#{danHao_huoHao_chengFenDaiMa}")
+    List<String> selectEbNoFromSapso(@Param("danHao_huoHao_chengFenDaiMa")
+                                             String danHao_huoHao_chengFenDaiMa);
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Select("select isnull(idx_no,'') as 'idxNo',name as 'idxName' from indx")
+    List<CategoryName> fenlei();
+
+
+    /**
+     * 得到最高层级的分类,其实只有一个
+     */
+    @Select("select isnull(idx_no,'') as 'idxNo',name as 'idxName' from indx where idx_up is null")
+    List<CategoryNameCode> getCommonder();
+
+    /**
+     * 得到下一级的分类,有多个
+     */
+    @Select("select isnull(idx_no,'') as 'idxNo',name as 'idxName' from indx where idx_up=#{indxNo}")
+    List<CategoryNameCode> getChildCategoryNameCode(@Param("indxNo") String indxNo);
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Select("select isnull(name,'') from prdt where idx1 in(select idx_no from indx where idx_no=#{idxNo})")
+    List<String> getCodeList(@Param("idxNo") String idxNo);
+
+    @Select("select isnull(sal_no,'') as 'salNo',isnull(name,'') as 'salName' from salm")
+    List<FuZeRen> fuZeRen();
+
+    @Select("select isnull(mark_no,'')as 'markNo',isnull(name,'')as 'name' from mark")
+    List<PinPai> pinPai();
+
+
+    List<String> selectDangQianYeSuoYouId
+            (@Param("dangQianYe") Integer dangQianYe
+                    , @Param("meiYeXianShiShu") Integer meiYeXianShiShu);
+
+    /**
+     * 得到某页分页id,打样未确认的
+     */
+    List<String> selectDangQianYeSuoYouIdNotConfirm
+    (@Param("dangQianYe") Integer dangQianYe,
+     @Param("meiYeXianShiShu") Integer meiYeXianShiShu);
+
+    /**
+     * 得到某页分页id,打样已经确认的
+     */
+    List<String> selectDangQianYeSuoYouIdAlReadyConfirm
+    (@Param("dangQianYe") Integer dangQianYe,
+     @Param("meiYeXianShiShu") Integer meiYeXianShiShu);
+
+
+    @Select("select count(id) from PRDT_SAMP")
+    Integer dangYangZongJiLuShu();
+
+
+
+
+    @Select("Select count(id) from prdt_samp where isconfirm=1")
     Integer getCountOfAllReadyConfirm();
 
-        @Select("Select count(id) from prdt_samp")
-        Integer getCountOfAll();
+    @Select("Select count(id) from prdt_samp")
+    Integer getCountOfAll();
 
-        @Select("Select count(id) from prdt_samp where isconfirm=0")
-        Integer getCountOfAllNotConfirm();
+    @Select("Select count(id) from prdt_samp where isconfirm=0")
+    Integer getCountOfAllNotConfirm();
 
 
-        @Update("update prdt_samp set thum=#{newthums} where thum like #{thumInDataBase}")
-        int updateThumColumn(@Param("thumInDataBase") String thumInDataBase, @Param("newthums") String newthums);
+    @Update("update prdt_samp set thum=#{newthums} where thum like #{thumInDataBase}")
+    int updateThumColumn(@Param("thumInDataBase") String thumInDataBase,
+                         @Param("newthums") String newthums);
 
-        @Update("update prdt_samp set attach=#{newattachs} where attach like #{attachInDataBase}")
-        int updateAttachColumn(@Param("attachInDataBase") String attachInDataBase, @Param("newattachs") String newattachs);
-        //test PageBean
-        @Select("Select count(id) from prdt_Samp")
-        int countItem();
-        //testPageBean
-        @Select("Select * from prdt_Samp order by id DESC")
-        List<PrdtSamp> findAll();
+    @Update("update prdt_samp set attach=#{newattachs} where attach like #{attachInDataBase}")
+    int updateAttachColumn(@Param("attachInDataBase") String attachInDataBase,
+                           @Param("newattachs") String newattachs);
 
-        @Select("select cur_id as curId,name as name from cur_id")
-        List<CurrentType> CurrentType();
-////////////////单位模块//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        @Select("SELECT DISTINCT UT FROM PRDT WHERE ISNULL(UT,'')!=''")
-        List<String> allUnit();//所有主单位
-        @Select("SELECT DISTINCT UT1 FROM PRDT WHERE ISNULL(UT1,'')!=''")
-        List<String> allUnit1();//所有副单位
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        @Select("select prd_no from prdt_samp where id=#{uuid}")
-        String selectPrdNoFromPrdtSamp(@Param("uuid") String uuid);
+    //test PageBean
+    @Select("Select count(id) from prdt_Samp")
+    int countItem();
+
+    //testPageBean
+    @Select("Select * from prdt_Samp order by id DESC")
+    List<PrdtSamp> findAll();
+
+    @Select("select cur_id as curId,name as name from cur_id")
+    List<CurrentType> CurrentType();
+
+    ////////////////单位模块//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Select("SELECT DISTINCT UT FROM PRDT WHERE ISNULL(UT,'')!=''")
+    List<String> allUnit();//所有主单位
+
+    @Select("SELECT DISTINCT UT1 FROM PRDT WHERE ISNULL(UT1,'')!=''")
+    List<String> allUnit1();//所有副单位
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Select("select prd_no from prdt_samp where id=#{uuid}")
+    String selectPrdNoFromPrdtSamp(@Param("uuid") String uuid);
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /**
-     *产品打样的时候,采购定价的时候,  prdno对应的在prdt中的主副单位判断和插入语句(一下4个语句)
-     * */
+     * 产品打样的时候,采购定价的时候,  prdno对应的在prdt中的主副单位判断和插入语句(一下4个语句)
+     */
     @Select({"SELECT TOP 1 UT FROM PRDT WHERE PRD_NO=#{prdNo}"})
     String selectUtFromPrdt(@Param("prdNo") String prdNo);
 
@@ -210,10 +230,10 @@ public interface ManyTabSerch {
     String selectUt1FromPrdt(@Param("prdNo") String prdNo);
 
     @Update({"UPDATE PRDT SET UT=#{unit} WHERE prd_no=#{prdNo}"})
-    Integer insertUnitToUtOfPrdt(@Param("unit") String unit,@Param("prdNo") String prdNo);
+    Integer insertUnitToUtOfPrdt(@Param("unit") String unit, @Param("prdNo") String prdNo);
 
     @Update({"UPDATE PRDT SET UT1=#{unit} WHERE prd_no=#{prdNo}"})
-    Integer insertUnitToUt1OfPrdt(@Param("unit") String unit,@Param("prdNo") String prdNo);
+    Integer insertUnitToUt1OfPrdt(@Param("unit") String unit, @Param("prdNo") String prdNo);
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -223,5 +243,7 @@ public interface ManyTabSerch {
 
 
     @Select({"select count(id) from prdt_samp where id=#{uuid}"})
-    Integer countByIdOfprdtSamp(@Param("uuid")String uuid);
+    Integer countByIdOfprdtSamp(@Param("uuid") String uuid);
+
+
 }
