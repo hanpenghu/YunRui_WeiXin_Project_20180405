73714378866332f949c25b2447037c1ce9f50eda
hanpenghu <563909408@qq.com>
2018-03-01 10:58:37 +0800

    2018_3_1   weekday(4)   10:58:35

--- src/main/java/com/winwin/picreport/Bcontroller/PicSearchPic/utils/DataSetSingleModifyExample1.java
+++ src/main/java/com/winwin/picreport/Bcontroller/PicSearchPic/utils/DataSetSingleModifyExample1.java
@@ -5,6 +5,7 @@ import cn.productai.api.core.enums.LanguageType;
 import cn.productai.api.core.exceptions.ClientException;
 import cn.productai.api.pai.entity.dataset.DataSetModifyResponse;
 import cn.productai.api.pai.entity.dataset.DataSetSingleAddByImageUrlRequest;
+import com.winwin.picreport.Futils.p;
 
 /**
  * Created by Zhong Wang on 2017/8/15.
@@ -16,8 +17,8 @@ public class DataSetSingleModifyExample1 {
 
     public DataSetModifyResponse  run(IWebClient client,String image_set_id,String imageUrl) {
 
-        System.out.println("==>  Demo - 向数据集增加单条数据  <==");
-        System.out.println("See https://api-doc.productai.cn/doc/pai.html#向数据集增加单条数据 for details.\r\n");
+        p.p("==>  Demo - 向数据集增加单条数据  <==");
+        p.p("See https://api-doc.productai.cn/doc/pai.html#向数据集增加单条数据 for details.\r\n");
         //参数1是image_set_id
         DataSetSingleAddByImageUrlRequest request = new DataSetSingleAddByImageUrlRequest(image_set_id,null,null);
 //        request.setImageUrl("http://test.waltercrow.co.nz/wp/wp-content/uploads/2010/06/muji-clothes.jpg");
@@ -26,27 +27,27 @@ public class DataSetSingleModifyExample1 {
         DataSetModifyResponse response =null;
         try {
            response = client.getResponse(request);
-            System.out.println("==============================Result==============================");
+            p.p("==============================Result==============================");
 
             // access the response directly
-            System.out.println(String.format("LastModifiedTime - %s", response.getLastModifiedTime()));
+            p.p(String.format("LastModifiedTime - %s", response.getLastModifiedTime()));
 
-            System.out.println("==============================Result==============================");
+            p.p("==============================Result==============================");
         } catch (cn.productai.api.core.exceptions.ServerException e) {
-            System.out.println(String.format("ServerException occurred. ErrorCode: %s \r\n ErrorMessage: %s",
+            p.p(String.format("ServerException occurred. ErrorCode: %s \r\n ErrorMessage: %s",
                     e.getErrorCode(),
                     e.getErrorMessage()));
             e.printStackTrace();
 
         } catch (ClientException e) {
-            System.out.println(String.format("ClientException occurred. ErrorCode: %s \r\n ErrorMessage: %s \r\n RequestId: %s",
+            p.p(String.format("ClientException occurred. ErrorCode: %s \r\n ErrorMessage: %s \r\n RequestId: %s",
                     e.getErrorCode(),
                     e.getErrorMessage(),
                     e.getRequestId()));
             e.printStackTrace();
 
         } catch (Exception e) {
-            System.out.println(String.format("%s occurred. ErrorMessage: %s", e.getClass().getName(), e.getMessage()));
+            p.p(String.format("%s occurred. ErrorMessage: %s", e.getClass().getName(), e.getMessage()));
             e.printStackTrace();
         }
         return response;
